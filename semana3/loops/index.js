ca27329fc8995bf6c7fe991c5f1c78e35e1d6552
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-04-23 12:24:46 -0300

    Exercicios feitos

---
+++ semana3/loops/index.js
@@ -0,0 +1,69 @@
+// ----------------------> Exercicio 1 <---------------------------
+
+//O código verifica se i é menor que 5, se sim, i recebe + 1 e valor recebe valor + i
+//Dessa forma, o loop que acontece até que i seja maior que 5
+//No console, o valor terá 10, já que soma com i toda vez que entra no loop
+
+// ----------------------> Exercicio 2 <---------------------------
+
+// Números de lista com valor maior que 18 serão impressos no console
+//Sim, para acessar cada indice,
+// precisariamos de uma variavel index que começa em 0 e recebe um a cada entrada entrada no for
+//Dessa forma, além do valor de numero, teriamos o index
+
+// ----------------------> Exercicio 3 <---------------------------
+
+//a.
+
+const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+let arrayPar = []
+
+// for (let numero of array) {
+// console.log(numero)
+// }
+
+//b.
+
+// for (let numero of array) {
+//     numero = numero /10
+//     console.log(numero)
+// }
+
+//c.
+
+
+// for (let numero of array){
+//     if(numero % 2 === 0){
+//         arrayPar.push(numero)
+//     }
+       
+// }
+
+// console.log(arrayPar)
+
+//d.
+// let index = 0
+
+// for (let numero of array) {
+//     console.log(`O elemento do índex ${index} é ${numero} `)
+//     index ++
+// }
+
+//e.
+
+// let maiorNumero = 0
+// let menorNumero = 0
+
+// for(let numero of array){
+    
+//     while(numero > maiorNumero){
+//         maiorNumero = numero
+//         menorNumero = maiorNumero
+//     }
+//     while(numero < menorNumero){
+//         menorNumero = numero
+//     }
+// }
+
+// console.log(`O maior número é ${maiorNumero} e o menor é ${menorNumero}`)
+
