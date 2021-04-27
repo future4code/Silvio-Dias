c33e9ffecb0b5722658e005ab93cd48a896b36dd
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-04-27 16:25:53 -0300

    Exercicio e desafio feitos

---
+++ semana4/planner/index.js
@@ -0,0 +1,50 @@
+let novaTarefa = ""
+let dia = ""
+let hora = 0
+let numeroTarefa = 0
+let tarefaArray = []
+
+function criarTarefa(){
+    //Cria a parte em código da Tarefa
+    let inputTarefa = document.getElementById('tarefa').value
+    let inputDia = document.getElementById('dias-semana').value
+    let inputHora = document.getElementById('horario').value
+    
+    novaTarefa = inputTarefa
+    dia = inputDia
+    hora = inputHora
+
+    
+    if(novaTarefa !== ""){
+    imprimirCalendario(novaTarefa,dia,hora)
+    document.getElementById("tarefa").value = ""
+    }else{
+        alert("Não é possível criar uma tarefa sem nome")
+    }
+}
+
+
+function imprimirCalendario(nomeTarefa,diaSemana,horaTarefa){
+    
+
+    let calendarioDia = document.getElementById(dia) //Busca o ID para imprimir no calendário 
+    let tarefaID = `tarefa${numeroTarefa}` //Cria uma ID única para cada entrada
+  
+
+    calendarioDia.innerHTML += `<p id="${tarefaID}" onclick="riscarTarefa('${tarefaID}')" class="tarefaListada"> - ${nomeTarefa} (${horaTarefa}) </p>`
+
+    tarefaArray.push(tarefaID) //Insere 
+    numeroTarefa ++ //Aumenta em um o código para criar ID unica
+}
+
+function riscarTarefa(id_tarefa){ //Recebe a ID no evento
+    document.getElementById(id_tarefa).className = "tarefaRiscada" //insere classe que risca o que foi clicado pela ID
+}
+
+function limparTarefas(){
+    for(let limpadroTarefa of tarefaArray){
+        document.getElementById(limpadroTarefa).innerHTML = ""
+    }
+
+    tarefaArray = []
+}
