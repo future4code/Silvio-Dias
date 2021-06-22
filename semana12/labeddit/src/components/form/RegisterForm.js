1dd96a99cf994a395713487ca64f59890ed451ca
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-06-22 12:26:56 -0300

    Cadastro funcionando

--- semana12/labeddit/src/components/form/RegisterForm.js
+++ semana12/labeddit/src/components/form/RegisterForm.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import useForm from '../../hooks/useForm'
 
-function RegisterForm() {
-    const {form, onChange} = useForm([{}])
+function RegisterForm(props) {
+    const {form, onChange} = useForm([])
     return (
         <div>
             <form>
@@ -10,6 +10,7 @@ function RegisterForm() {
                 <input name = "email" onChange =  {onChange} type = "email" placeholder = "E-mail"/>
                 <input name = "password" onChange = {onChange} type = "password" placeholder = "Password"/>
             </form>
+            <button onClick = {() => props.register(form)} >Cadastrar</button>
         </div>
     )
 }
--- semana12/labeddit/src/screens/screenRegister.js
+++ semana12/labeddit/src/screens/screenRegister.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import RegisterForm from '../components/form/RegisterForm'
+import axios from "axios"
+import { BASE_URL } from '../constants/url'
 
 function ScreenRegister() {
+
+    const register = (form) => {
+        console.log(form)
+         axios
+        .post(`${BASE_URL}/users/signup`,form)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((err) => {
+            alert(err.message)
+        })
+    }
     return (
         <div>
-            <RegisterForm/>
+            <RegisterForm
+            register = {register}
+            
+            />
         </div>
     )
 }
