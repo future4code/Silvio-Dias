94fccedb687a7726a164a2ea9b54c61ee1e338ca
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-06-23 17:54:45 -0300

    Melhor organização de card

---
+++ semana12/labeddit/src/components/card/Card.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { CardPost } from './style'
+
+function Card({ post },props) {
+    const {id,title,body,
+            commentCount,voteSum,userVote,
+            username} = post
+
+    const history = useHistory()
+        console.log(props.singlePost)
+    return (
+        <CardPost key = {id}>
+            <div>
+                <h2>{title}</h2>
+            </div>
+            <div>
+                <p>{body}</p>
+            </div>
+            <div>
+                <h3>{username}</h3>
+            </div>
+
+            <div>
+                {commentCount && (<p>Comentários: {commentCount}</p>)}
+                {userVote && (<p>Avalialão: {voteSum}</p>)} 
+            </div>
+       
+        </CardPost>
+    )
+}
+
+export default Card
